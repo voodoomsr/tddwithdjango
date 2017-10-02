989655a0ba40beed4a4f2b553c283a74e063d6ec
Michael Soza <mjsr@outlookcom>
2017-10-02 00:28:51 -0300

    Added test to check page value after POST

--- functional_tests.py
+++ functional_tests.py
@@ -27,6 +27,8 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,7 +4,14 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 
+import re
+
 class HomePageTest(TestCase):
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -14,4 +21,11 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(self.remove_csrf(response.content.decode()), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
